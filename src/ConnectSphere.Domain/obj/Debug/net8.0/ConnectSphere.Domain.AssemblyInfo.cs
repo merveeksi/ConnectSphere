94fbab0c97bfe46b7c4561d45fc40deee2e31909
MERVE EKŞİ <merveeksii61@gmail.com>
2024-12-29 20:44:51 +0300

    ..

--- src/ConnectSphere.Domain/obj/Debug/net8.0/ConnectSphere.Domain.AssemblyInfo.cs
+++ src/ConnectSphere.Domain/obj/Debug/net8.0/ConnectSphere.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectSphere.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+868917a4cd2ce90aa1a3de38ba00c541bb8e57e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8157472ba4f9e2d55a445de0774520c31a15a948")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectSphere.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectSphere.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/ConnectSphere.WebApi/obj/Debug/net8.0/ConnectSphere.WebApi.AssemblyInfo.cs
+++ src/ConnectSphere.WebApi/obj/Debug/net8.0/ConnectSphere.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectSphere.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f12233c6d5416bac218a28e0507984dbaee2f79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8157472ba4f9e2d55a445de0774520c31a15a948")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectSphere.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectSphere.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
