22f8d5714d7f635b074416b6a47e20511a3478d0
MERVE EKŞİ <merveeksii61@gmail.com>
2024-12-31 22:00:19 +0300

    ...

--- src/ConnectSphere.Infrastructure/obj/Debug/net8.0/ConnectSphere.Infrastructure.AssemblyInfo.cs
+++ src/ConnectSphere.Infrastructure/obj/Debug/net8.0/ConnectSphere.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectSphere.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10866fdbb6a15e2b789d6abeada07aae4fa91dc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ac8f9f17700127e8af3254f0e79f643e5221716")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectSphere.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectSphere.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
